9031b4eab4819281309de06d4b0e04394911ec97
gaelcolas <gaelcolas@synedgy.com>
2019-09-10 09:20:29 +0800

    moving submodule under Modules folder

--- Sampler/suffix.ps1
+++ Sampler/suffix.ps1
@@ -1,6 +1,7 @@
 # Inspired from https://github.com/nightroman/Invoke-Build/blob/64f3434e1daa806814852049771f4b7d3ec4d3a3/Tasks/Import/README.md#example-2-import-from-a-module-with-tasks
 Get-ChildItem (Join-Path -Path $PSScriptRoot -ChildPath 'tasks') | ForEach-Object {
-    $taskFileAliasName = "$($_.BaseName).Sampler.ib.tasks"
+    $ModuleName = ([io.FileInfo]$MyInvocation.MyCommand.Name).BaseName
+    $taskFileAliasName = "$($_.BaseName).$ModuleName.ib.tasks"
     Set-Alias -Name $taskFileAliasName -Value $_.FullName
     Export-ModuleMember -Alias $taskFileAliasName
 }
