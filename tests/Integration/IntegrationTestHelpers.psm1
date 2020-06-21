5d8cc119e07e85d8871c1e74285e3988d30c2493
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-06-21 16:31:30 +0200

    Fix command tree

---
+++ tests/Integration/IntegrationTestHelpers.psm1
@@ -0,0 +1,38 @@
+function Install-TreeCommand
+{
+    if (-not (Get-Command -Name 'tree' -ErrorAction 'SilentlyContinue'))
+    {
+        if ($IsMacOS)
+        {
+            brew install tree
+        }
+
+        if ($IsLinux)
+        {
+            apt-get install tree
+        }
+    }
+}
+
+function Get-DirectoryTree
+{
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param ()
+
+    $treeOutput = switch ($true)
+        {
+            { $IsLinux -or $IsMacOS }
+            {
+                tree -a $mockModuleRootPath
+            }
+
+            # Assume Windows
+            default
+            {
+                tree /f $mockModuleRootPath
+            }
+        }
+
+    return $treeOutput
+}
