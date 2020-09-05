8670d2a63a16231de0a72cfcd56f1075a1edf0c8
gaelcolas <gaelcolas@synedgy.com>
2020-09-05 15:55:17 +0200

    updated help

--- Sampler/Public/New-SampleModule.ps1
+++ Sampler/Public/New-SampleModule.ps1
@@ -1,5 +1,62 @@
+<#
+.SYNOPSIS
+Create a module scaffolding and add samples & build pipeline.
+
+.DESCRIPTION
+New-SampleModule helps you bootstrap your PowerShell module project by
+creating a the folder structure of your module, and optionally add the
+pipeline files to help with compiling the module, publishing to PSGallery
+and GitHub and testing quality and style such as per the DSC Community
+guildelines.
+
+.PARAMETER DestinationPath
+Destination of your module source root folder, defaults to the current directory ".".
+We assume that your current location is the module folder, and within this folder we
+will find the source folder, the tests folder and other supporting files.
+
+.PARAMETER ModuleType
+Preset of module you would like to create:
+    - SimpleModule_NoBuild
+    - SimpleModule
+    - CompleteModule
+    - CompleteModule_NoBuild
+    - dsccommunity
+
+.PARAMETER ModuleAuthor
+The author of module that will be populated in the Module Manifest and will show in the Gallery.
+
+.PARAMETER ModuleName
+The Name of your Module.
+
+.PARAMETER ModuleDescription
+The Description of your Module, to be used in your Module manifest.
+
+.PARAMETER CustomRepo
+The Custom PS repository if you want to use an internal (private) feed to pull for dependencies.
+
+.PARAMETER ModuleVersion
+Version you want to set in your Module Manfest. If you follow our approach, this will be updated during compilation anyway.
+
+.PARAMETER LicenseType
+Type of license you would like to add to your repository. We recommend MIT for Open Source projects.
+
+.PARAMETER SourceDirectory
+How you would like to call your Source repository to differentiate from the output and the tests folder. We recommend to call it Source.
+
+.PARAMETER Features
+If you'd rather select specific features this template offers to build your module, use this parameter set instead.
+
+.EXAMPLE
+C:\src> New-SampleModule -DestinationPath . -ModuleType CompleteModule -ModuleAuthor "Gael Colas" -ModuleName MyModule -ModuleVersion 0.0.1 -ModuleDescription "a sample module" -LicenseType MIT -SourceDirectory Source
+
+.NOTES
+See Add-Sample to add elements such as functions (private or public), tests, DSC Resources to your project.
+#>
 function New-SampleModule
 {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '')]
+
     [CmdletBinding(DefaultParameterSetName = 'ByModuleType')]
     [OutputType([void])]
     param (
