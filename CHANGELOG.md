10b1bf39648ac0b044db6af93912cf217623ac7a
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-09-12 14:22:46 +0200

    More code cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Updated code style to match the DSC Community style guideline.
 - Updated logic that handles the installation on PSDepend in the bootstrap
   file `Resolve-Dependency.ps1`.
+- Updated year in LICENSE.
   
 ### Fixed
 
--- Sampler/suffix.ps1
+++ Sampler/suffix.ps1
@@ -1,8 +1,10 @@
 # Inspired from https://github.com/nightroman/Invoke-Build/blob/64f3434e1daa806814852049771f4b7d3ec4d3a3/Tasks/Import/README.md#example-2-import-from-a-module-with-tasks
-Get-ChildItem (Join-Path -Path $PSScriptRoot -ChildPath 'tasks\*') -Include '*.build.*' |
-    ForEach-Object {
-        $ModuleName = ([IO.FileInfo] $MyInvocation.MyCommand.Name).BaseName
+Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'tasks\*') -Include '*.build.*' |
+    ForEach-Object -Process {
+        $ModuleName = ([System.IO.FileInfo] $MyInvocation.MyCommand.Name).BaseName
         $taskFileAliasName = "$($_.BaseName).$ModuleName.ib.tasks"
+
         Set-Alias -Name $taskFileAliasName -Value $_.FullName
+
         Export-ModuleMember -Alias $taskFileAliasName
     }
