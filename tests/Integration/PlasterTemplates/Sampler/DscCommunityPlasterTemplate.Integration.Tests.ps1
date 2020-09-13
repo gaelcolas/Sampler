dd34eb684fa0293c3368bc4d2f588c241630ca37
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-09-13 20:40:26 +0200

    Update unit tests

--- tests/Integration/PlasterTemplates/Sampler/DscCommunityPlasterTemplate.Integration.Tests.ps1
+++ tests/Integration/PlasterTemplates/Sampler/DscCommunityPlasterTemplate.Integration.Tests.ps1
@@ -32,6 +32,7 @@ Describe 'DSC Community Plaster Template' {
             $listOfExpectedFilesAndFolders = @(
                 # Folders (relative to module root)
 
+                '.github'
                 '.vscode'
                 'source'
                 'source/en-US'
@@ -42,11 +43,17 @@ Describe 'DSC Community Plaster Template' {
                 # Files (relative to module root)
 
                 '.gitattributes'
+                '.github/ISSUE_TEMPLATE'
+                '.github/PULL_REQUEST_TEMPLATE.md'
+                '.github/ISSUE_TEMPLATE/General.md'
+                '.github/ISSUE_TEMPLATE/Problem_with_resource.md'
+                '.github/ISSUE_TEMPLATE/Resource_proposal.md'
                 '.gitignore'
                 '.markdownlint.json'
                 'azure-pipelines.yml'
                 'build.ps1'
                 'build.yaml'
+                'CHANGELOG.md'
                 'CODE_OF_CONDUCT.md'
                 'codecov.yml'
                 'CONTRIBUTING.md'
