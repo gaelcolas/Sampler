ffb5aded1444757eb6c45770460cf0579fd92c22
gaelcolas <gaelcolas@synedgy.com>
2021-03-24 13:07:09 +0100

    updating changelog and tasks from discussion

--- .build/tasks/generateHelp.PlatyPS.build.ps1
+++ .build/tasks/generateHelp.PlatyPS.build.ps1
@@ -14,7 +14,7 @@ param
 
     [Parameter()]
     [System.String]
-    $HelpFolder = (property HelpFolder 'help'),
+    $HelpSourceFolder = (property HelpSourceFolder 'docs'),
 
     [Parameter()]
     [System.String]
@@ -34,10 +34,20 @@ param
 
     [Parameter()]
     [cultureInfo]
-    $HelpCultureInfo = 'en-US'
+    $HelpCultureInfo = 'en-US',
+
+    [Parameter()]
+    [System.Management.Automation.SwitchParameter]
+    $CopyHelpMamlToBuiltModuleBase = (property CopyHelpMamlToBuiltModuleBase $True),
+
+    # Build Configuration object
+    [Parameter()]
+    [System.Collections.Hashtable]
+    $BuildInfo = (property BuildInfo @{ })
 )
 
-Task Generate_md_help_from_module {
+# Synopsis: Generate MAML from the built module (and add to module Base).
+Task Generate_maml_from_built_module {
     if ([System.String]::IsNullOrEmpty($ProjectName))
     {
         $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
@@ -48,10 +58,12 @@ Task Generate_md_help_from_module {
         $SourcePath = Get-SamplerSourcePath -BuildRoot $BuildRoot
     }
 
+    $ProjectPath = Get-SamplerAbsolutePath -Path $ProjectPath -RelativeTo $BuildRoot
     $OutputDirectory = Get-SamplerAbsolutePath -Path $OutputDirectory -RelativeTo $BuildRoot
     $PesterOutputFolder = Get-SamplerAbsolutePath -Path $PesterOutputFolder -RelativeTo $OutputDirectory
 
     "`tProject Name             = '$ProjectName'"
+    "`tProject Path             = '$ProjectPath'"
     "`tOutput Directory         = '$OutputDirectory'"
     "`tPester Output Folder     = '$PesterOutputFolder"
 
@@ -77,9 +89,19 @@ Task Generate_md_help_from_module {
     }
 
     $builtModuleBase = Get-SamplerBuiltModuleBase @GetBuiltModuleManifestParams
+    if ($builtModuleBase)
+    {
+        $builtModuleBase = Get-Item -Path $builtModuleBase -ErrorAction 'SilentlyContinue'
+    }
+
     "`tBuilt Module Base        = '$builtModuleBase'"
 
     $builtModuleManifest = Get-SamplerBuiltModuleManifest @GetBuiltModuleManifestParams
+    if ($builtModuleManifest)
+    {
+        $builtModuleManifest = Get-Item -Path $builtModuleManifest -ErrorAction 'SilentlyContinue'
+    }
+
     "`tBuilt Module Manifest    = '$builtModuleManifest'"
 
     if ($builtModuleRootScriptPath = Get-SamplerModuleRootPath -ModuleManifestPath $builtModuleManifest)
@@ -98,8 +120,8 @@ Task Generate_md_help_from_module {
     "`tModule Version Folder    = '$ModuleVersionFolder'"
     "`tPre-release Tag          = '$preReleaseTag'"
 
-    $HelpFolder = Get-SamplerAbsolutePath -Path $HelpFolder -RelativeTo $SourcePath
-    "`tHelp Folder Path         = '$HelpFolder'"
+    $HelpSourceFolder = Get-SamplerAbsolutePath -Path $HelpSourceFolder -RelativeTo $ProjectPath
+    "`tHelp Folder Path         = '$HelpSourceFolder'"
 
     $HelpOutputFolder =  Get-SamplerAbsolutePath -Path $HelpOutputFolder -RelativeTo $OutputDirectory
     "`tHelp output Folder        = '$HelpOutputFolder'"
@@ -110,23 +132,51 @@ Task Generate_md_help_from_module {
     $HelpOutputCultureFolder = Get-SamplerAbsolutePath -Path $HelpCultureInfo -RelativeTo $HelpOutputVersionFolder
     "`tHelp output Culture path  = '$HelpOutputCultureFolder'"
 
-    $generateHelpCommands = @(
-        '$env:PSModulePath = "{0}"' -f $Env:PSModulePath
-        'Import-Module -Name "{0}" -Force' -f $ProjectName
-        '$newMarkdownHelpParams = @{{ Module = "{0}"; OutputFolder = "{1}"; AlphabeticParamsOrder = ${2}; WithModulePage = ${3}; ExcludeDontShow = ${4}; Force = $true}}' -f $ProjectName, $HelpOutputCultureFolder, $true, $true, $true
-        'New-MarkdownHelp @newMarkdownHelpParams'
-        'New-ExternalHelp -Path "{0}" -OutputPath "{1}" -Force' -f $HelpFolder, $HelpOutputCultureFolder
-    )
+    $DocOutputFolder = Get-SamplerAbsolutePath -Path 'docs' -RelativeTo $OutputDirectory
+    "`tDocs output folder path   = '$DocOutputFolder'"
+
+    $null = [bool]::TryParse($CopyHelpMamlToBuiltModuleBase, [ref]$CopyHelpMamlToBuiltModuleBase)
+    "`Copy MAML to Built Module  = '$CopyHelpMamlToBuiltModuleBase'"
+
+    $AlphabeticParamOrder = $true
+    $WithModulePage = $true
+    $ExcludeDontShow = $true
+
+    $generateHelpCommands = @"
+    `$env:PSModulePath = '$Env:PSModulePath'
+    `$targetModule = Import-Module -Name '$ProjectName' -ErrorAction Stop -Passthru
+
+    `$newMarkdownHelpParams = @{
+        Module                = '$ProjectName'
+        OutputFolder          = '$DocOutputFolder'
+        AlphabeticParamsOrder = `$$AlphabeticParamOrder
+        WithModulePage        = `$$WithModulePage
+        ExcludeDontShow       = `$$ExcludeDontShow
+        Force                 = `$true
+        ErrorAction           = 'Stop'
+        Locale                = '$HelpCultureInfo'
+        HelpVersion           = '$ModuleVersion'
+    }
+
+    New-MarkdownHelp @newMarkdownHelpParams
+    New-ExternalHelp -Path "$DocOutputFolder" -OutputPath "$HelpOutputCultureFolder" -Force
+"@
 
     Write-Build -Color DarkGray -Text "$generateHelpCommands"
-    $sb = [ScriptBlock]::create(($generateHelpCommands -join "`r`n"))
-    Write-Build -Color DarkGray -Text "$sb"
+    $sb = [ScriptBlock]::create($generateHelpCommands)
 
     $pwshPath = (Get-Process -Id $PID).Path
-    &$pwshPath -c $sb -ep 'ByPass'
+    &$pwshPath -Command $sb -ExecutionPolicy 'ByPass'
+
+    if ($CopyHelpMamlToBuiltModuleBase)
+    {
+        # Copy the created MAML to the ModuleBase root folder (not in Locale as there's only 1 language)
+        Get-ChildItem -Path $HelpOutputCultureFolder -ErrorAction 'SilentlyContinue' | Copy-Item -Destination $builtModuleBase -Force
+    }
 }
 
-Task UpdateHelp {
+# Synopsis: Generate (if absent) or Update the Markdown help source files for each locale folder (i.e. docs/en-US).
+Task update_markdown_help_source {
     if ([System.String]::IsNullOrEmpty($ProjectName))
     {
         $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
@@ -137,10 +187,12 @@ Task UpdateHelp {
         $SourcePath = Get-SamplerSourcePath -BuildRoot $BuildRoot
     }
 
+    $ProjectPath = Get-SamplerAbsolutePath -Path $ProjectPath -RelativeTo $BuildRoot
     $OutputDirectory = Get-SamplerAbsolutePath -Path $OutputDirectory -RelativeTo $BuildRoot
     $PesterOutputFolder = Get-SamplerAbsolutePath -Path $PesterOutputFolder -RelativeTo $OutputDirectory
 
     "`tProject Name             = '$ProjectName'"
+    "`tProject Path             = '$ProjectPath'"
     "`tOutput Directory         = '$OutputDirectory'"
     "`tPester Output Folder     = '$PesterOutputFolder"
 
@@ -187,23 +239,65 @@ Task UpdateHelp {
     "`tModule Version Folder    = '$ModuleVersionFolder'"
     "`tPre-release Tag          = '$preReleaseTag'"
 
-    $HelpFolder = Get-SamplerAbsolutePath -Path $HelpFolder -RelativeTo $SourcePath
-    "`tHelp Folder Path         = '$HelpFolder'"
+    $HelpSourceFolder = Get-SamplerAbsolutePath -Path $HelpSourceFolder -RelativeTo $ProjectPath
+    "`tHelp Folder Path         = '$HelpSourceFolder'"
 
-    $generateHelpCommands = @(
-        '$env:PSModulePath = "{0}"' -f $Env:PSModulePath
-        'Import-Module -Name "{0}" -Force' -f $ProjectName
-        'Update-MarkdownHelpModule -Path "{0}" -Force' -f $HelpFolder
-    )
+    $existingLocaleFolders = (Get-ChildItem -Path $HelpSourceFolder -Directory -ErrorAction 'SilentlyContinue').Name
 
-    $sb = [ScriptBlock]::create(($generateHelpCommands -join "`r`n"))
-    Write-Build -Color DarkGray -Text "$sb"
+    if ($existingLocaleFolders.count -le 0)
+    {
+        Write-Build -Color 'Yellow' -Text "No existing help locale folder found. Adding default 'en-US'"
+        $existingLocaleFolders = @('en-US')
+    }
 
-    $pwshPath = (Get-Process -Id $PID).Path
-    &$pwshPath -c $sb -ep 'ByPass'
+    foreach ($Locale in $ExistingLocaleFolders)
+    {
+        $HelpSourceLocaleFolder = Join-Path -Path $HelpSourceFolder -ChildPath $Locale
+        Write-Build -Color 'Yellow' -Text "Updating help for locale '$Locale'"
+
+        $AlphabeticParamOrder = $true
+        $WithModulePage = $true
+        $ExcludeDontShow = $true
+        $MarkdownHelpMetadataAsString = '@{GeneratedBy = "Sampler update_help_source task"}'
+
+        $generateHelpCommands = @"
+            `$env:PSModulePath = '$Env:PSModulePath'
+            `$targetModule = Import-Module -Name '$ProjectName' -ErrorAction Stop -Passthru
+
+            try
+            {
+                Update-MarkdownHelpModule -Path '$HelpSourceLocaleFolder' -Force -ErrorAction 'Stop'
+            }
+            catch
+            {
+                `$newMarkdownHelpParams = @{
+                    Module                = '$ProjectName'
+                    OutputFolder          = '$HelpSourceLocaleFolder'
+                    AlphabeticParamsOrder = `$$AlphabeticParamOrder
+                    WithModulePage        = `$$WithModulePage
+                    ExcludeDontShow       = `$$ExcludeDontShow
+                    Force                 = `$true
+                    ErrorAction           = 'Stop'
+                    Locale                = '$Locale'
+                    HelpVersion           = '$ModuleVersion'
+                    Metadata              = $MarkdownHelpMetadataAsString
+                }
+
+                New-MarkdownHelp @newMarkdownHelpParams
+            }
+"@
+
+        Write-Build -Color DarkGray -Text $generateHelpCommands
+        $sb = [ScriptBlock]::create($generateHelpCommands)
+        Write-Build -Color DarkGray -Text "$sb"
+
+        $pwshPath = (Get-Process -Id $PID).Path
+        &$pwshPath -Command $sb -ExecutionPolicy 'ByPass'
+    }
 }
 
-Task Generate_Maml_From_Md {
+# Synopsis: Generates the MAML for each Locale found under the Help source folder (i.e. docs/en-US).
+Task Generate_MAML_From_Markdown_help_source {
     if ([System.String]::IsNullOrEmpty($ProjectName))
     {
         $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
@@ -214,10 +308,12 @@ Task Generate_Maml_From_Md {
         $SourcePath = Get-SamplerSourcePath -BuildRoot $BuildRoot
     }
 
+    $ProjectPath = Get-SamplerAbsolutePath -Path $ProjectPath -RelativeTo $BuildRoot
     $OutputDirectory = Get-SamplerAbsolutePath -Path $OutputDirectory -RelativeTo $BuildRoot
     $PesterOutputFolder = Get-SamplerAbsolutePath -Path $PesterOutputFolder -RelativeTo $OutputDirectory
 
     "`tProject Name             = '$ProjectName'"
+    "`tProject Path             = '$ProjectPath'"
     "`tOutput Directory         = '$OutputDirectory'"
     "`tPester Output Folder     = '$PesterOutputFolder"
 
@@ -243,9 +339,19 @@ Task Generate_Maml_From_Md {
     }
 
     $builtModuleBase = Get-SamplerBuiltModuleBase @GetBuiltModuleManifestParams
+    if ($builtModuleBase)
+    {
+        $builtModuleBase = Get-Item -Path $builtModuleBase -ErrorAction 'SilentlyContinue'
+    }
+
     "`tBuilt Module Base        = '$builtModuleBase'"
 
     $builtModuleManifest = Get-SamplerBuiltModuleManifest @GetBuiltModuleManifestParams
+    if ($builtModuleManifest)
+    {
+        $builtModuleManifest = Get-Item -Path $builtModuleManifest -ErrorAction 'SilentlyContinue'
+    }
+
     "`tBuilt Module Manifest    = '$builtModuleManifest'"
 
     if ($builtModuleRootScriptPath = Get-SamplerModuleRootPath -ModuleManifestPath $builtModuleManifest)
@@ -264,8 +370,8 @@ Task Generate_Maml_From_Md {
     "`tModule Version Folder    = '$ModuleVersionFolder'"
     "`tPre-release Tag          = '$preReleaseTag'"
 
-    $HelpFolder = Get-SamplerAbsolutePath -Path $HelpFolder -RelativeTo $SourcePath
-    "`tHelp Folder Path         = '$HelpFolder'"
+    $HelpSourceFolder = Get-SamplerAbsolutePath -Path $HelpSourceFolder -RelativeTo $ProjectPath
+    "`tHelp Folder Path         = '$HelpSourceFolder'"
 
     $HelpOutputFolder =  Get-SamplerAbsolutePath -Path $HelpOutputFolder -RelativeTo $OutputDirectory
     "`tHelp output Folder        = '$HelpOutputFolder'"
@@ -273,18 +379,40 @@ Task Generate_Maml_From_Md {
     $HelpOutputVersionFolder = Get-SamplerAbsolutePath -Path $ModuleVersion -RelativeTo $HelpOutputFolder
     "`tHelp output Version Folder= '$HelpOutputVersionFolder'"
 
-    $HelpOutputCultureFolder = Get-SamplerAbsolutePath -Path $HelpCultureInfo -RelativeTo $HelpOutputVersionFolder
-    "`tHelp output Culture path  = '$HelpOutputCultureFolder'"
+    $null = [bool]::TryParse($CopyHelpMamlToBuiltModuleBase, [ref]$CopyHelpMamlToBuiltModuleBase)
+    "`Copy MAML to Built Module  = '$CopyHelpMamlToBuiltModuleBase'"
 
-    $generateHelpCommands = @(
-        '$env:PSModulePath = "{0}"' -f $Env:PSModulePath
-        'New-ExternalHelp -Path "{0}" -OutputPath "{1}" -Force' -f $HelpFolder, $HelpOutputCultureFolder
-    )
+    $existingLocaleFolders = (Get-ChildItem -Path $HelpSourceFolder -Directory -ErrorAction 'SilentlyContinue').Name
 
-    $sb = [ScriptBlock]::create(($generateHelpCommands -join "`r`n"))
-    Write-Build -Color DarkGray -Text "$sb"
+    # if the docs folder exists, but it has no subfolders, try to find md files directly within docs/
+    if ($existingLocaleFolders.count -le 0 -and (Test-Path -Path $HelpSourceFolder))
+    {
+        Write-Build -Color 'Yellow' -Text "No existing help locale folder found. Trying '$HelpSourceFolder'"
+        $existingLocaleFolders = @('')
+    }
 
-    $pwshPath = (Get-Process -Id $PID).Path
-    &$pwshPath -c $sb -ep 'ByPass'
-    # pwsh -NoProfile -Command $sb
+    foreach ($Locale in $ExistingLocaleFolders)
+    {
+        $HelpSourceLocaleFolder = Join-Path -Path $HelpSourceFolder -ChildPath $Locale
+        Write-Build -Color 'Yellow' -Text "Generating MAML for locale '$Locale' if markdown is present."
+
+        $HelpOutputCultureFolder = Join-Path -Path $HelpOutputVersionFolder -ChildPath $Locale
+
+        $generateHelpCommands = @(
+            '$env:PSModulePath = "{0}"' -f $Env:PSModulePath
+            'New-ExternalHelp -Path "{0}" -OutputPath "{1}" -Force' -f $HelpSourceLocaleFolder, $HelpOutputCultureFolder
+        )
+
+        $sb = [ScriptBlock]::create(($generateHelpCommands -join "`r`n"))
+        Write-Build -Color DarkGray -Text "$sb"
+
+        $pwshPath = (Get-Process -Id $PID).Path
+        &$pwshPath -Command $sb -ExecutionPolicy 'ByPass'
+    }
+
+    if ($CopyHelpMamlToBuiltModuleBase)
+    {
+        Get-ChildItem -Path $HelpOutputVersionFolder -ErrorAction 'SilentlyContinue' |
+            Copy-Item -Recurse -Destination $builtModuleBase -Force
+    }
 }
