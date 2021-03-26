1af78e7016d1dc53393f386db9daad7aba6538fd
gaelcolas <gaelcolas@synedgy.com>
2021-03-26 15:51:25 +0100

    deduplicating var init code for PlatyPS tasks

--- .build/tasks/Clean.ModuleBuilder.build.ps1
+++ .build/tasks/Clean.ModuleBuilder.build.ps1
@@ -9,7 +9,7 @@ param (
     $RequiredModulesDirectory = (property RequiredModulesDirectory $(Join-Path $OutputDirectory 'RequiredModules'))
 )
 
-# Removes the BuildOutput\modules (errors if Pester is loaded)
+# Removes the OutputDirectory\modules (errors if Pester is loaded)
 task CleanAll Clean, CleanModule
 
 # Synopsis: Deleting the content of the Build Output folder, except ./modules
@@ -22,7 +22,7 @@ task Clean {
     Get-ChildItem -Path $OutputDirectory -Exclude $FolderToExclude | Remove-Item -Force -Recurse
 }
 
-# Synopsis: Removes the Modules from BuildOutput\Modules folder, might fail if there's an handle on one file.
+# Synopsis: Removes the Modules from OutputDirectory\Modules folder, might fail if there's an handle on one file.
 task CleanModule {
     $OutputDirectory =  Get-SamplerAbsolutePath -Path $OutputDirectory -RelativeTo $BuildRoot
 
