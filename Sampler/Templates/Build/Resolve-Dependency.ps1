42939e8b7fc536e20a9ad06523343e908d395044
phbits <phbits@outlook.com>
2021-04-11 20:40:32 +0000

    fix MinimumPSDependVersion comparison to use System.Version

--- Sampler/Templates/Build/Resolve-Dependency.ps1
+++ Sampler/Templates/Build/Resolve-Dependency.ps1
@@ -290,7 +290,7 @@ try
 
     if ($PSBoundParameters.ContainsKey('MinimumPSDependVersion'))
     {
-        $psDependModule = $psDependModule | Where-Object -Property -eq $MinimumPSDependVersion
+        $psDependModule = $psDependModule | Where-Object { $_.Version -ge $([System.Version]::new($MinimumPSDependVersion)) }
     }
 
     if (-not $psDependModule)
