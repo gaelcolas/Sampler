d135f94b3e069cbe52093cfff11915edae49acf8
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-04-25 12:55:19 +0200

    Add public function Format-Xml

---
+++ Sampler/Public/Format-Xml.ps1
@@ -0,0 +1,54 @@
+
+<#
+    .SYNOPSIS
+        Formats an XML document and returns a string.
+
+    .DESCRIPTION
+        Formats an XML document and returns a string.
+
+    .PARAMETER XmlDocument
+        The XML document to format.
+
+    .PARAMETER Indented
+        Specifies if the XML document should be formatted with indentation.
+
+    .EXAMPLE
+        Format-Xml -XmlDocument '<?xml version="1.0"?><a><b /></a>' -Indented
+#>
+function Format-Xml
+{
+    [OutputType([System.String])]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.Xml.XmlDocument]
+        $XmlDocument,
+
+        [Parameter()]
+        [Switch]
+        $Indented
+    )
+
+    $xmlSettings = New-Object -TypeName 'System.Xml.XmlWriterSettings'
+
+    if ($Indented.IsPresent)
+    {
+        $xmlSettings.Indent = $true
+    }
+    else
+    {
+        $xmlSettings.Indent = $false
+    }
+
+    $xmlOutput = New-Object -TypeName 'System.Text.StringBuilder'
+
+    $xmlWriter = [System.Xml.XmlWriter]::Create($xmlOutput, $xmlSettings)
+
+    $XmlDocument.Save($xmlWriter)
+
+    $XmlWriter.Flush()
+    $xmlWriter.Close()
+
+    return $xmlOutput.ToString()
+}
---
+++ tests/Unit/Public/Format-Xml.tests.ps1
@@ -0,0 +1,37 @@
+$ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
+$ProjectName = ((Get-ChildItem -Path $ProjectPath\*\*.psd1).Where{
+        ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
+        $(try
+            {
+                Test-ModuleManifest $_.FullName -ErrorAction Stop
+            }
+            catch
+            {
+                $false
+            } )
+    }).BaseName
+
+Import-Module $ProjectName
+
+Describe 'Format-Xml' {
+    BeforeAll {
+        $mockXmlDocument = '<?xml version="1.0" encoding="utf-16" standalone="no"?><a><b /></a>'
+    }
+
+    It 'Should format the XML without indentation' {
+        $result = Sampler\Format-Xml -XmlDocument $mockXmlDocument
+
+        $result | Should -Be $mockXmlDocument
+    }
+
+    It 'Should format the XML with indentation' {
+        $result = Sampler\Format-Xml -XmlDocument $mockXmlDocument -Indented
+
+        ($result -replace '\r?\n', "`n") | Should -Be @"
+<?xml version="1.0" encoding="utf-16" standalone="no"?>
+<a>
+  <b />
+</a>
+"@ -replace '\r?\n', "`n"
+    }
+}
