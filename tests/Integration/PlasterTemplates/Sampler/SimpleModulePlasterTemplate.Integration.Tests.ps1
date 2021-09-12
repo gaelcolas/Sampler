956b71f46f2c62bdc6d4146bb4922921a13ae46d
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-09-12 13:39:39 +0200

    Fix tests

--- tests/Integration/PlasterTemplates/Sampler/SimpleModulePlasterTemplate.Integration.Tests.ps1
+++ tests/Integration/PlasterTemplates/Sampler/SimpleModulePlasterTemplate.Integration.Tests.ps1
@@ -117,4 +117,190 @@ Describe 'Simple Module Plaster Template' {
             $TreeStructureIsOk | Should -BeTrue -Because $report
         }
     }
+
+    Context 'When creating a new module project that is not using GitVersion' {
+        BeforeAll {
+            $mockModuleName = 'ModuleDsc'
+            $mockModuleRootPath = Join-Path -Path $TestDrive -ChildPath $mockModuleName
+
+            $listOfExpectedFilesAndFolders = @(
+                # Folders (relative to module root)
+
+                '.vscode'
+                'source'
+                'source/en-US'
+                'source/Private'
+                'source/Public'
+                'tests'
+                'tests/QA'
+                'output'
+                'output/RequiredModules'
+
+                # Files (relative to module root)
+
+                '.gitattributes'
+                '.gitignore'
+                'azure-pipelines.yml'
+                'build.ps1'
+                'build.yaml'
+                'CHANGELOG.md'
+                'codecov.yml'
+                'README.md'
+                'RequiredModules.psd1'
+                'Resolve-Dependency.ps1'
+                'Resolve-Dependency.psd1'
+                '.vscode/tasks.json'
+                'source/ModuleDsc.psd1'
+                'source/ModuleDsc.psm1'
+                'source/en-US/about_ModuleDsc.help.txt'
+                'tests/QA/module.tests.ps1'
+            )
+        }
+
+        It 'Should create a new module without throwing' {
+            $invokePlasterParameters = @{
+                TemplatePath         = Join-Path -Path $importedModule.ModuleBase -ChildPath 'Templates/Sampler'
+                DestinationPath      = $TestDrive
+                SourceDirectory      = 'source'
+                NoLogo               = $true
+                Force                = $true
+
+                # Template
+                ModuleType           = 'SimpleModule'
+
+                # Template properties
+                ModuleName           = $mockModuleName
+                ModuleAuthor         = 'SamplerTestUser'
+                ModuleDescription    = 'Module description'
+                ModuleVersion        = '1.0.0'
+                CustomRepo           = 'PSGallery'
+                MainGitBranch        = 'main'
+                UpstreamOrganization = 'AccountName'
+                UseGitVersion        = $false
+                UseCodeCovIo         = $true
+            }
+
+            { Invoke-Plaster @invokePlasterParameters } | Should -Not -Throw
+        }
+
+        It 'Should have the expected folder and file structure' {
+            $modulePaths = Get-ChildItem -Path $mockModuleRootPath -Recurse -Force
+
+            # Make the path relative to module root.
+            $relativeModulePaths = $modulePaths.FullName -replace [RegEx]::Escape($mockModuleRootPath)
+
+            # Change to slash when testing on Windows.
+            $relativeModulePaths = ($relativeModulePaths -replace '\\', '/').TrimStart('/')
+
+            # check files & folders discrepencies
+            $missingFilesOrFolders    = $listOfExpectedFilesAndFolders.Where{$_ -notin $relativeModulePaths}
+            $unexpectedFilesAndFolders  = $relativeModulePaths.Where{$_ -notin $listOfExpectedFilesAndFolders}
+            $TreeStructureIsOk = ($missingFilesOrFolders.count -eq 0 -and $unexpectedFilesAndFolders.count -eq 0)
+
+            # format the report to be used in because
+            $report = ":`r`n  Missing:`r`n`t$($missingFilesOrFolders -join "`r`n`t")`r`n  Unexpected:`r`n`t$($unexpectedFilesAndFolders -join "`r`n`t")`r`n."
+
+            # Check if tree structure failed. If so output the module directory tree.
+            if ( -not $TreeStructureIsOk)
+            {
+                $treeOutput = Get-DirectoryTree -Path $mockModuleRootPath
+                Write-Verbose -Message ($treeOutput | Out-String) -Verbose
+            }
+
+            $TreeStructureIsOk | Should -BeTrue -Because $report
+        }
+    }
+
+    Context 'When creating a new module project that is not using Codecov.io' {
+        BeforeAll {
+            $mockModuleName = 'ModuleDsc'
+            $mockModuleRootPath = Join-Path -Path $TestDrive -ChildPath $mockModuleName
+
+            $listOfExpectedFilesAndFolders = @(
+                # Folders (relative to module root)
+
+                '.vscode'
+                'source'
+                'source/en-US'
+                'source/Private'
+                'source/Public'
+                'tests'
+                'tests/QA'
+                'output'
+                'output/RequiredModules'
+
+                # Files (relative to module root)
+
+                '.gitattributes'
+                '.gitignore'
+                'azure-pipelines.yml'
+                'build.ps1'
+                'build.yaml'
+                'CHANGELOG.md'
+                'GitVersion.yml'
+                'README.md'
+                'RequiredModules.psd1'
+                'Resolve-Dependency.ps1'
+                'Resolve-Dependency.psd1'
+                '.vscode/tasks.json'
+                'source/ModuleDsc.psd1'
+                'source/ModuleDsc.psm1'
+                'source/en-US/about_ModuleDsc.help.txt'
+                'tests/QA/module.tests.ps1'
+            )
+        }
+
+        It 'Should create a new module without throwing' {
+            $invokePlasterParameters = @{
+                TemplatePath         = Join-Path -Path $importedModule.ModuleBase -ChildPath 'Templates/Sampler'
+                DestinationPath      = $TestDrive
+                SourceDirectory      = 'source'
+                NoLogo               = $true
+                Force                = $true
+
+                # Template
+                ModuleType           = 'SimpleModule'
+
+                # Template properties
+                ModuleName           = $mockModuleName
+                ModuleAuthor         = 'SamplerTestUser'
+                ModuleDescription    = 'Module description'
+                ModuleVersion        = '1.0.0'
+                CustomRepo           = 'PSGallery'
+                MainGitBranch        = 'main'
+                UpstreamOrganization = 'AccountName'
+                UseGitVersion        = $true
+                UseCodeCovIo         = $false
+            }
+
+            { Invoke-Plaster @invokePlasterParameters } | Should -Not -Throw
+        }
+
+        It 'Should have the expected folder and file structure' {
+            $modulePaths = Get-ChildItem -Path $mockModuleRootPath -Recurse -Force
+
+            # Make the path relative to module root.
+            $relativeModulePaths = $modulePaths.FullName -replace [RegEx]::Escape($mockModuleRootPath)
+
+            # Change to slash when testing on Windows.
+            $relativeModulePaths = ($relativeModulePaths -replace '\\', '/').TrimStart('/')
+
+            # check files & folders discrepencies
+            $missingFilesOrFolders    = $listOfExpectedFilesAndFolders.Where{$_ -notin $relativeModulePaths}
+            $unexpectedFilesAndFolders  = $relativeModulePaths.Where{$_ -notin $listOfExpectedFilesAndFolders}
+            $TreeStructureIsOk = ($missingFilesOrFolders.count -eq 0 -and $unexpectedFilesAndFolders.count -eq 0)
+
+            # format the report to be used in because
+            $report = ":`r`n  Missing:`r`n`t$($missingFilesOrFolders -join "`r`n`t")`r`n  Unexpected:`r`n`t$($unexpectedFilesAndFolders -join "`r`n`t")`r`n."
+
+            # Check if tree structure failed. If so output the module directory tree.
+            if ( -not $TreeStructureIsOk)
+            {
+                $treeOutput = Get-DirectoryTree -Path $mockModuleRootPath
+                Write-Verbose -Message ($treeOutput | Out-String) -Verbose
+            }
+
+            $TreeStructureIsOk | Should -BeTrue -Because $report
+        }
+    }
 }
