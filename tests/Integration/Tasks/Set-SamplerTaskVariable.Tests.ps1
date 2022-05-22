6ba61162d77f886919d399fe6c941b4ef4866d39
Johan Ljunggren <johan.ljunggren@shamn.se>
2022-05-22 13:15:54 +0200

    Fix unit test

--- tests/Integration/Tasks/Set-SamplerTaskVariable.Tests.ps1
+++ tests/Integration/Tasks/Set-SamplerTaskVariable.Tests.ps1
@@ -10,17 +10,9 @@ BeforeAll {
 
     # Re-import the module using force to get any code changes between runs.
     Import-Module -Name $script:moduleName -Force -ErrorAction 'Stop'
-
-    $PSDefaultParameterValues['InModuleScope:ModuleName'] = $script:moduleName
-    #$PSDefaultParameterValues['Mock:ModuleName'] = $script:moduleName
-    #$PSDefaultParameterValues['Should:ModuleName'] = $script:moduleName
 }
 
 AfterAll {
-    #$PSDefaultParameterValues.Remove('Mock:ModuleName')
-    $PSDefaultParameterValues.Remove('InModuleScope:ModuleName')
-    #$PSDefaultParameterValues.Remove('Should:ModuleName')
-
     Remove-Module -Name $script:moduleName
 }
 
