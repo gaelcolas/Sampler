ac2c9bc90635b7b8f098c68897994a77fb296239
Johan Ljunggren <johan.ljunggren@shamn.se>
2022-06-25 17:49:17 +0200

    Fix unit test

--- tests/Unit/tasks/Invoke-Pester.pester.build.Tests.ps1
+++ tests/Unit/tasks/Invoke-Pester.pester.build.Tests.ps1
@@ -244,3 +244,295 @@ Describe 'Invoke_Pester_Tests_v5' {
         }
     }
 }
+
+Describe 'Fail_Build_If_Pester_Tests_Failed' {
+    BeforeAll {
+        # Dot-source mocks
+        . $PSScriptRoot/../TestHelpers/MockSetSamplerTaskVariable
+
+        $taskAlias = Get-Alias -Name 'Invoke-Pester.pester.build.Sampler.ib.tasks'
+
+        $mockTaskParameters = @{
+            OutputDirectory = Join-Path -Path $TestDrive -ChildPath 'output'
+            ProjectName = 'MyModule'
+        }
+    }
+
+    Context 'When tests failed' {
+        BeforeAll {
+            Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                return 70
+            }
+
+            Mock -CommandName Test-Path -ParameterFilter {
+                $Path -match 'PesterObject_'
+            } -MockWith {
+                return $true
+            }
+
+            Mock -CommandName Import-Clixml -MockWith {
+                return @{
+                    FailedCount = 10
+                }
+            }
+        }
+
+        It 'Should throw the correct error' {
+            {
+                Invoke-Build -Task 'Fail_Build_If_Pester_Tests_Failed' -File $taskAlias.Definition @mockTaskParameters
+            } | Should -Throw -ExpectedMessage 'Assertion failed. Failed 10 tests. Aborting Build'
+        }
+    }
+
+    Context 'When tests failed' {
+        BeforeAll {
+            Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                return 70
+            }
+
+            Mock -CommandName Test-Path -ParameterFilter {
+                $Path -match 'PesterObject_'
+            } -MockWith {
+                return $true
+            }
+
+            Mock -CommandName Import-Clixml -MockWith {
+                return @{
+                    FailedCount = 0
+                }
+            }
+        }
+
+        It 'hould run the build task without throwing' {
+            {
+                Invoke-Build -Task 'Fail_Build_If_Pester_Tests_Failed' -File $taskAlias.Definition @mockTaskParameters
+            } | Should -Not -Throw
+        }
+    }
+}
+
+Describe 'Pester_If_Code_Coverage_Under_Threshold' {
+    BeforeAll {
+        # Dot-source mocks
+        . $PSScriptRoot/../TestHelpers/MockSetSamplerTaskVariable
+
+        $taskAlias = Get-Alias -Name 'Invoke-Pester.pester.build.Sampler.ib.tasks'
+
+        $mockTaskParameters = @{
+            OutputDirectory = Join-Path -Path $TestDrive -ChildPath 'output'
+            ProjectName = 'MyModule'
+        }
+    }
+
+    Context 'When running Pester 5' {
+        Context 'When code coverage threshold was not met' {
+            BeforeAll {
+                Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                    return 70
+                }
+
+                Mock -CommandName Test-Path -ParameterFilter {
+                    $Path -match 'PesterObject_'
+                } -MockWith {
+                    return $true
+                }
+
+                Mock -CommandName Import-Clixml -MockWith {
+                    return @{
+                        Version = '5.3.3'
+                        CodeCoverage = @{
+                            CoveragePercent = 40
+                        }
+                    }
+                }
+            }
+
+            It 'Should run the build task without throwing' {
+                {
+                    Invoke-Build -Task 'Pester_If_Code_Coverage_Under_Threshold' -File $taskAlias.Definition @mockTaskParameters
+                } | Should -Throw -ExpectedMessage 'Code Coverage FAILURE: 40 % is under the threshold of 70 %.'
+            }
+        }
+
+        Context 'When meeting code coverage threshold' {
+            BeforeAll {
+                Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                    return 70
+                }
+
+                Mock -CommandName Test-Path -ParameterFilter {
+                    $Path -match 'PesterObject_'
+                } -MockWith {
+                    return $true
+                }
+
+                Mock -CommandName Import-Clixml -MockWith {
+                    return @{
+                        Version = '5.3.3'
+                        CodeCoverage = @{
+                            CoveragePercent = 75
+                        }
+                    }
+                }
+            }
+
+            It 'Should run the build task without throwing' {
+                {
+                    Invoke-Build -Task 'Pester_If_Code_Coverage_Under_Threshold' -File $taskAlias.Definition @mockTaskParameters
+                } | Should -Not -Throw
+            }
+        }
+    }
+
+    Context 'When running Pester 4' {
+        Context 'When code coverage threshold was not met' {
+            BeforeAll {
+                Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                    return 70
+                }
+
+                Mock -CommandName Test-Path -ParameterFilter {
+                    $Path -match 'PesterObject_'
+                } -MockWith {
+                    return $true
+                }
+
+                Mock -CommandName Import-Clixml -MockWith {
+                    return @{
+                        CodeCoverage = @{
+                            NumberOfCommandsExecuted = 20
+                            NumberOfCommandsAnalyzed = 40
+                        }
+                    }
+                }
+            }
+
+            It 'Should run the build task without throwing' {
+                {
+                    Invoke-Build -Task 'Pester_If_Code_Coverage_Under_Threshold' -File $taskAlias.Definition @mockTaskParameters
+                } | Should -Throw -ExpectedMessage 'Code Coverage FAILURE: 50 % is under the threshold of 70 %.'
+            }
+        }
+
+        Context 'When meeting code coverage threshold' {
+            BeforeAll {
+                Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                    return 70
+                }
+
+                Mock -CommandName Test-Path -ParameterFilter {
+                    $Path -match 'PesterObject_'
+                } -MockWith {
+                    return $true
+                }
+
+                Mock -CommandName Import-Clixml -MockWith {
+                    return @{
+                        CodeCoverage = @{
+                            NumberOfCommandsExecuted = 40
+                            NumberOfCommandsAnalyzed = 50
+                        }
+                    }
+                }
+            }
+
+            It 'Should run the build task without throwing' {
+                {
+                    Invoke-Build -Task 'Pester_If_Code_Coverage_Under_Threshold' -File $taskAlias.Definition @mockTaskParameters
+                } | Should -Not -Throw
+            }
+        }
+    }
+}
+
+Describe 'Pester_Run_Times' {
+    BeforeAll {
+        # Dot-source mocks
+        . $PSScriptRoot/../TestHelpers/MockSetSamplerTaskVariable
+
+        $taskAlias = Get-Alias -Name 'Invoke-Pester.pester.build.Sampler.ib.tasks'
+
+        $mockTaskParameters = @{
+            OutputDirectory = Join-Path -Path $TestDrive -ChildPath 'output'
+            ProjectName = 'MyModule'
+        }
+    }
+
+    Context 'When running Pester 5' {
+        Context 'When code coverage threshold was not met' {
+            BeforeAll {
+                Mock -CommandName Get-Module -MockWith {
+                    return @{
+                        Version = '4.1.10'
+                    }
+                }
+            }
+
+            It 'Should run the build task without throwing' {
+                {
+                    Invoke-Build -Task 'Pester_Run_Times' -File $taskAlias.Definition @mockTaskParameters
+                } | Should -Not -Throw
+            }
+        }
+    }
+
+    Context 'When running Pester 5' {
+        Context 'When code coverage threshold was not met' {
+            BeforeAll {
+                Mock -CommandName Get-Module -MockWith {
+                    return @{
+                        Version = '5.3.3'
+                    }
+                }
+
+                Mock -CommandName Get-CodeCoverageThreshold -MockWith {
+                    return 70
+                }
+
+                Mock -CommandName Test-Path -ParameterFilter {
+                    $Path -match 'PesterObject_'
+                } -MockWith {
+                    return $true
+                }
+
+                Mock -CommandName Import-Clixml -MockWith {
+                    return @{
+                        Version = '5.3.3'
+                        Duration = New-TimeSpan -Seconds 10
+                        TotalCount = 21
+                        Containers = @(
+                            @{
+                                Duration = New-TimeSpan -Seconds 5
+                                Result = 'Passed'
+                                PassedCount = 10
+                                FailedCount = 0
+                                SkippedCount = 0
+                                TotalCount = 10
+                                Item = @{
+                                    Name = 'TestScript1'
+                                }
+                            }
+                            @{
+                                Duration = New-TimeSpan -Seconds 5
+                                Result = 'Failed'
+                                PassedCount = 10
+                                FailedCount = 1
+                                SkippedCount = 0
+                                TotalCount = 1
+                                Item = @{
+                                    Name = 'TestScript2'
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+
+            It 'Should run the build task without throwing' {
+                {
+                    Invoke-Build -Task 'Pester_Run_Times' -File $taskAlias.Definition @mockTaskParameters
+                } | Should -Not -Throw
+            }
+        }
+    }
+}
