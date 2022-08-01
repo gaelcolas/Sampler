gaelcolas/Sampler

commit dade014db4c6109ca64f249497a3dcf0a9504283

Add more tests to raise coverage (#383)
:shipit:

Sampler/scripts/Set-SamplerTaskVariable.ps1
@@ -175,18 +175,11 @@ else
     }
 
     $BuiltModuleManifest = Get-SamplerBuiltModuleManifest @GetBuiltModuleManifestParams
-    if ($BuiltModuleManifest)
-    {
-        $BuiltModuleManifest = (Get-Item -Path $BuiltModuleManifest -ErrorAction 'SilentlyContinue').FullName
-    }
 
     "`tBuilt Module Manifest      = '$BuiltModuleManifest'"
 
     $BuiltModuleBase = Get-SamplerBuiltModuleBase @GetBuiltModuleManifestParams
-    if ($BuiltModuleBase)
-    {
-        $BuiltModuleBase = (Get-Item -Path $BuiltModuleBase -ErrorAction 'SilentlyContinue').FullName
-    }
+
 
     "`tBuilt Module Base          = '$BuiltModuleBase'"
 
@@ -206,11 +199,6 @@ else
     if ($BuiltModuleManifest)
     {
         $BuiltModuleRootScriptPath = Get-SamplerModuleRootPath -ModuleManifestPath $BuiltModuleManifest
-
-        if ($BuiltModuleRootScriptPath)
-        {
-            $BuiltModuleRootScriptPath = (Get-Item -Path $BuiltModuleRootScriptPath -ErrorAction 'SilentlyContinue').FullName
-        }
     }
 
     "`tBuilt Module Root Script   = '$BuiltModuleRootScriptPath'"

tests/Unit/TestHelpers/MockSetSamplerTaskVariable.ps1
@@ -1,0 +1,93 @@
+<#
+    .SYNOPSIS
+        This script is dot-source'd into the unit test for Set-SamplerTasksVariable
+        and also the unit tests for the build tasks.
+
+    .NOTES
+        This mocks the Set-SamplerTasksVariable.ps1 variables to:
+
+        $BuiltModuleBase = "$TestDrive/output/builtModule/MyModule/2.0.0"
+        $BuiltModuleManifest = "$TestDrive/output/builtModule/MyModule/2.0.0/MyModule.psd1"
+        $BuiltModuleRootScriptPath = "$TestDrive/output/builtModule/MyModule/2.0.0/MyModule.psm1"
+        $BuiltModuleSubDirectory = "$TestDrive/output/builtModule"
+        $ChocolateyBuildOutput = "$TestDrive/output/choco"
+        $ModuleManifestPath = "$TestDrive/source/MyModule.psd1"
+        $ModuleVersion = '2.0.0'
+        $ModuleVersionFolder = '2.0.0'
+        $OutputDirectory = "$TestDrive/output"
+        $ProjectName = 'MyModule'
+        $ReleaseNotesPath = "$TestDrive/output/"
+        $SourcePath = "$TestDrive/source"
+        $VersionedOutputDirectory = $true
+#>
+
+$BuildRoot = $TestDrive
+
+$ProjectName = $null
+$SourcePath = $null
+$OutputDirectory = 'output'
+$ReleaseNotesPath = $null
+$BuiltModuleSubDirectory = 'builtModule'
+$ModuleManifestPath = $null
+$ChocolateyBuildOutput = 'choco'
+$VersionedOutputDirectory = $true
+$BuiltModuleManifest = $null
+$BuiltModuleBase = $null
+$ModuleVersion = $null
+$ModuleVersionFolder = $null
+$BuiltModuleRootScriptPath = $null
+
+Mock -CommandName Get-SamplerProjectName -MockWith {
+    return 'MyModule'
+}
+
+Mock -CommandName Get-SamplerSourcePath -MockWith {
+    return (Join-Path -Path $TestDrive -ChildPath 'source')
+}
+
+Mock -CommandName Get-SamplerAbsolutePath -ParameterFilter {
+    $Path -eq 'MyModule.psd1'
+} -MockWith {
+    return (
+        Join-Path -Path $TestDrive -ChildPath 'source' |
+            Join-Path -ChildPath 'MyModule.psd1'
+    )
+}
+
+Mock -CommandName Get-SamplerBuiltModuleManifest -MockWith {
+    return (
+        Join-Path -Path $TestDrive -ChildPath 'output' |
+            Join-Path -ChildPath 'builtModule' |
+            Join-Path -ChildPath 'MyModule' |
+            Join-Path -ChildPath '2.0.0' |
+            Join-Path -ChildPath 'MyModule.psd1'
+    )
+}
+
+Mock -CommandName Get-SamplerBuiltModuleBase -MockWith {
+    return (
+        Join-Path -Path $TestDrive -ChildPath 'output' |
+            Join-Path -ChildPath 'builtModule' |
+            Join-Path -ChildPath 'MyModule' |
+            Join-Path -ChildPath '2.0.0'
+    )
+}
+
+Mock -CommandName Get-BuiltModuleVersion -MockWith {
+    return '2.0.0'
+}
+
+Mock -CommandName Get-SamplerModuleRootPath -MockWith {
+    return (
+        Join-Path -Path $TestDrive -ChildPath 'output' |
+            Join-Path -ChildPath 'builtModule' |
+            Join-Path -ChildPath 'MyModule' |
+            Join-Path -ChildPath '2.0.0' |
+            Join-Path -ChildPath 'MyModule.psm1'
+    )
+}
+
+# This is only used when calling Set-SamplerTaskVariable with parameter -AsNewBuild
+Mock -CommandName Get-BuildVersion -MockWith {
+    return '2.0.0'
+}

commit 7314abfdd2f05d38135d1ed33c13418da47740e5

Fix bug introduced with PR #383

Sampler/scripts/Set-SamplerTaskVariable.ps1
@@ -176,10 +176,21 @@ else
 
     $BuiltModuleManifest = Get-SamplerBuiltModuleManifest @GetBuiltModuleManifestParams
 
+    # Resolve path to replace '*' with version number.
+    if ($BuiltModuleManifest)
+    {
+        $BuiltModuleManifest = (Get-Item -Path $BuiltModuleManifest -ErrorAction 'SilentlyContinue').FullName
+    }
+
     "`tBuilt Module Manifest      = '$BuiltModuleManifest'"
 
     $BuiltModuleBase = Get-SamplerBuiltModuleBase @GetBuiltModuleManifestParams
 
+    # Resolve path to replace '*' with version number.
+    if ($BuiltModuleBase)
+    {
+        $BuiltModuleBase = (Get-Item -Path $BuiltModuleBase -ErrorAction 'SilentlyContinue').FullName
+    }
 
     "`tBuilt Module Base          = '$BuiltModuleBase'"
 
@@ -199,6 +210,12 @@ else
     if ($BuiltModuleManifest)
     {
         $BuiltModuleRootScriptPath = Get-SamplerModuleRootPath -ModuleManifestPath $BuiltModuleManifest
+
+        # Resolve path to replace '*' with version number.
+        if ($BuiltModuleRootScriptPath)
+        {
+            $BuiltModuleRootScriptPath = (Get-Item -Path $BuiltModuleRootScriptPath -ErrorAction 'SilentlyContinue').FullName
+        }
     }
 
     "`tBuilt Module Root Script   = '$BuiltModuleRootScriptPath'"

tests/Unit/TestHelpers/MockSetSamplerTaskVariable.ps1
@@ -54,37 +54,67 @@ Mock -CommandName Get-SamplerAbsolutePath -ParameterFilter {
     )
 }
 
+$script:mockGetSamplerBuiltModuleManifestReturnValue = Join-Path -Path $TestDrive -ChildPath 'output' |
+    Join-Path -ChildPath 'builtModule' |
+    Join-Path -ChildPath 'MyModule' |
+    Join-Path -ChildPath '2.0.0' |
+    Join-Path -ChildPath 'MyModule.psd1'
+
 Mock -CommandName Get-SamplerBuiltModuleManifest -MockWith {
-    return (
-        Join-Path -Path $TestDrive -ChildPath 'output' |
-            Join-Path -ChildPath 'builtModule' |
-            Join-Path -ChildPath 'MyModule' |
-            Join-Path -ChildPath '2.0.0' |
-            Join-Path -ChildPath 'MyModule.psd1'
-    )
+    return $script:mockGetSamplerBuiltModuleManifestReturnValue
 }
 
+# This is called after the mock of Get-SamplerBuiltModuleManifest
+Mock -CommandName Get-Item -MockWith {
+    return @{
+        FullName = $script:mockGetSamplerBuiltModuleManifestReturnValue
+    }
+} -ParameterFilter {
+    # Must be the same path that the mock for Get-SamplerBuiltModuleManifest returns.
+    $Path -contains $script:mockGetSamplerBuiltModuleManifestReturnValue
+}
+
+$script:mockGetSamplerBuiltModuleBaseReturnValue = Join-Path -Path $TestDrive -ChildPath 'output' |
+    Join-Path -ChildPath 'builtModule' |
+    Join-Path -ChildPath 'MyModule' |
+    Join-Path -ChildPath '2.0.0'
+
 Mock -CommandName Get-SamplerBuiltModuleBase -MockWith {
-    return (
-        Join-Path -Path $TestDrive -ChildPath 'output' |
-            Join-Path -ChildPath 'builtModule' |
-            Join-Path -ChildPath 'MyModule' |
-            Join-Path -ChildPath '2.0.0'
-    )
+    return $script:mockGetSamplerBuiltModuleBaseReturnValue
+}
+
+# This is called after the mock of Get-SamplerBuiltModuleBase
+Mock -CommandName Get-Item -MockWith {
+    @{
+        FullName = $script:mockGetSamplerBuiltModuleBaseReturnValue
+    }
+} -ParameterFilter {
+    # Must be the same path that the mock for Get-SamplerBuiltModuleManifest returns.
+    $Path -contains $script:mockGetSamplerBuiltModuleBaseReturnValue
 }
 
 Mock -CommandName Get-BuiltModuleVersion -MockWith {
     return '2.0.0'
 }
 
+$script:mockGetSamplerModuleRootPathReturnValue = Join-Path -Path $TestDrive -ChildPath 'output' |
+    Join-Path -ChildPath 'builtModule' |
+    Join-Path -ChildPath 'MyModule' |
+    Join-Path -ChildPath '2.0.0' |
+    Join-Path -ChildPath 'MyModule.psm1'
+
 Mock -CommandName Get-SamplerModuleRootPath -MockWith {
-    return (
-        Join-Path -Path $TestDrive -ChildPath 'output' |
-            Join-Path -ChildPath 'builtModule' |
-            Join-Path -ChildPath 'MyModule' |
-            Join-Path -ChildPath '2.0.0' |
-            Join-Path -ChildPath 'MyModule.psm1'
-    )
+    return $script:mockGetSamplerModuleRootPathReturnValue
+}
+
+# This is called after the mock of Get-SamplerModuleRootPath
+Mock -CommandName Get-Item -MockWith {
+    @{
+        FullName = $script:mockGetSamplerModuleRootPathReturnValue
+    }
+} -ParameterFilter {
+    # Must be the same path that the mock for Get-SamplerBuiltModuleManifest returns.
+    $Path -contains $script:mockGetSamplerModuleRootPathReturnValue
 }
 
 # This is only used when calling Set-SamplerTaskVariable with parameter -AsNewBuild