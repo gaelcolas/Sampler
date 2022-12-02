7095f8bf2e969f880c5b25371441731b0fa9dc7a
Raimund Andree <r.andree@live.com>
2022-12-02 17:39:05 +0100

    Removed unneeded parameters

--- .build/tasks/SetPsModulePath.build.ps1
+++ .build/tasks/SetPsModulePath.build.ps1
@@ -5,50 +5,7 @@ param
     $BuiltModuleSubdirectory = (property BuiltModuleSubdirectory ''),
 
     [Parameter()]
-    [System.Management.Automation.SwitchParameter]
-    $VersionedOutputDirectory = (property VersionedOutputDirectory $true),
-
-    [Parameter()]
-    [System.String]
-    $ProjectName = (property ProjectName ''),
-
-    [Parameter()]
-    [System.String]
-    $SourcePath = (property SourcePath ''),
-
-    [Parameter()]
-    $ChangelogPath = (property ChangelogPath 'CHANGELOG.md'),
-
-    [Parameter()]
-    $ReleaseNotesPath = (property ReleaseNotesPath (Join-Path $OutputDirectory 'ReleaseNotes.md')),
-
-    [Parameter()]
-    [string]
-    $GitHubToken = (property GitHubToken ''), # retrieves from Environment variable
-
-    [Parameter()]
-    [string]
-    $ReleaseBranch = (property ReleaseBranch 'main'),
-
-    [Parameter()]
-    [string]
-    $GitHubConfigUserEmail = (property GitHubConfigUserEmail ''),
-
-    [Parameter()]
-    [string]
-    $GitHubConfigUserName = (property GitHubConfigUserName ''),
-
-    [Parameter()]
-    $GitHubFilesToAdd = (property GitHubFilesToAdd ''),
-
-    [Parameter()]
-    $BuildInfo = (property BuildInfo @{ }),
-
-    [Parameter()]
-    $SkipPublish = (property SkipPublish ''),
-
-    [Parameter()]
-    $MainGitBranch = (property MainGitBranch 'main')
+    $BuildInfo = (property BuildInfo @{ })
 )
 
 <#
