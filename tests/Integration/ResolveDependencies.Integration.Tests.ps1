f344ef3a08eb7dad258d7fc221709a144b2b582d
Johan Ljunggren <johan.ljunggren@shamn.se>
2023-09-04 16:00:22 +0200

    Handle pre-releases

--- tests/Integration/ResolveDependencies.Integration.Tests.ps1
+++ tests/Integration/ResolveDependencies.Integration.Tests.ps1
@@ -99,5 +99,58 @@ Describe 'Resolve dependencies' {
 
             $buildError | Should -BeNullOrEmpty
         }
+
+        Context 'When RequiredModules.psd1 contain pre-releases' {
+            It 'Should resolve dependencies without throwing' {
+                # Running in separate job so that we do not mess up the current session.
+                Start-Job -ScriptBlock {
+                    Set-Location $using:PWD
+
+                    Remove-Item -Path '.\RequiredModules.psd1' -Force
+
+                    $mockRequiredModulesDataFile = @'
+@{
+    PSDependOptions             = @{
+        AddToPath  = $true
+        Target     = 'output\RequiredModules'
+        Parameters = @{}
+    }
+
+    Pester                         = @{
+        Version    = '5.5.0-rc1'
+        Parameters = @{
+            AllowPrerelease = $true
+        }
+    }
+
+    InvokeBuild                 = 'latest'
+    PSScriptAnalyzer            = 'latest'
+    Plaster                     = 'latest'
+    ModuleBuilder               = 'latest'
+    MarkdownLinkCheck           = 'latest'
+    ChangelogManagement         = 'latest'
+    'Sampler.GitHubTasks'       = 'latest'
+    'DscResource.Test'          = 'latest'
+    'DscResource.AnalyzerRules' = 'latest'
+    xDscResourceDesigner        = 'latest'
+
+    PlatyPS = 'latest'
+}
+'@
+
+                    $mockRequiredModulesDataFile | Out-File -Path '.\RequiredModules.psd1' -Encoding UTF8 -NoClobber -NoNewline
+
+                    ./build.ps1 -ResolveDependency -Tasks 'noop' -UsePSResourceGet 4>&1 5>&1 6>&1 > $null
+                } |
+                    Receive-Job -Wait -AutoRemoveJob -ErrorVariable buildError
+
+                $buildError | Should -BeNullOrEmpty
+
+                $pesterModuleManifest = Import-PowerShellDataFile -Path '.\output\RequiredModules\Pester\**\Pester.psd1'
+
+                $pesterModuleManifest.ModuleVersion | Should -Be '5.5.0'
+                $pesterModuleManifest.PrivateData.PSData.Prerelease | Should -Be 'rc1'
+            }
+        }
     }
 }
