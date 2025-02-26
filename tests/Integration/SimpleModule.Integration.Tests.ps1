a38e3254500cd424c281552f35247c6f34969b9a
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-02-26 21:29:21 +0100

    Fix integ tests

--- tests/Integration/SimpleModule.Integration.Tests.ps1
+++ tests/Integration/SimpleModule.Integration.Tests.ps1
@@ -78,12 +78,14 @@ Describe 'SimpleModule' {
             git add . 2>&1
             git commit --message=first
 
-            ./build.ps1 -ResolveDependency -Tasks 'build' 4>&1 5>&1 6>&1 > $null
+            ./build.ps1 -ResolveDependency -Tasks 'noop' 4>&1 5>&1 6>&1 > $null
 
             # This is a workaround for the issue: https://github.com/PoshCode/ModuleBuilder/pull/136
-            Install-PSResource -Name Viscalyx.Common -Repository PSGallery -TrustRepository -Quiet -Confirm:$false
+            Install-PSResource -Name 'Viscalyx.Common' -Repository PSGallery -TrustRepository -Quiet -Confirm:$false
             Import-Module -Name Viscalyx.Common
-            Install-ModulePatch -SkipHashValidation -Uri https://raw.githubusercontent.com/viscalyx/Viscalyx.Common/refs/heads/main/patches/ModuleBuilder_3.1.7_patch.json -Force
+            Install-ModulePatch -SkipHashValidation -Uri 'https://raw.githubusercontent.com/viscalyx/Viscalyx.Common/refs/heads/main/patches/ModuleBuilder_3.1.7_patch.json' -Force
+
+            ./build.ps1 -Tasks 'build' 4>&1 5>&1 6>&1 > $null
         } |
             Receive-Job -Wait -AutoRemoveJob -ErrorVariable buildError
 
