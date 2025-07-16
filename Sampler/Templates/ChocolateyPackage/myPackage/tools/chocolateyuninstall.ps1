6d6d5e1a20e205cab5f47c80fdede0e691320ca9
ahpooch <81159041+ahpooch@users.noreply.github.com>
2025-07-16 18:59:36 +0300

    style: Typo in build.ps1. Standardized usage or Write-* cmdlets. (#530)

--- Sampler/Templates/ChocolateyPackage/myPackage/tools/chocolateyuninstall.ps1
+++ Sampler/Templates/ChocolateyPackage/myPackage/tools/chocolateyuninstall.ps1
@@ -73,7 +73,7 @@ if ($key.Count -eq 1)
 }
 elseif ($key.Count -eq 0)
 {
-    Write-Warning "$packageName has already been uninstalled by other means."
+    Write-Warning -Message "$packageName has already been uninstalled by other means."
 }
 elseif ($key.Count -gt 1)
 {
